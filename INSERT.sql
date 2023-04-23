441141b8de72ca204b50f6ab97f458d1bccc4333
Archit <101202058+archttt@users.noreply.github.com>
2023-04-23 21:15:40 +0530

    Add files via upload

---
+++ INSERT.sql
@@ -0,0 +1,27 @@
+insert into employee values (1, 'Archit', 'M','archit@gmail.com', '1/1/2019');
+insert into employee values (2, 'Saloni','F', 'saloni@gmail.com', '1/1/2019');
+insert into employee values (3, 'Riya','F', 'riya@gmail.com', '1/1/2019');
+insert into employee values (4, 'Arsh','M', 'arsh@gmail.com', '1/1/2019');
+insert into employee values (5, 'Arjit','M', 'arjit@yahoo.com', '1/1/2019');
+
+
+
+insert into salary values(1, 18000,5000);
+insert into salary values(2, 20000,5000);
+insert into salary values(3, 22000,6000);
+insert into salary values(4, 35000,6500);
+insert into salary values(5, 50000,7000);
+
+
+insert into employee_salary values(1,1);
+insert into employee_salary values(2, 3);
+insert into employee_salary values(3,5);
+insert into employee_salary values(4,2);
+insert into employee_salary values(5,1);
+
+
+insert into leave values(1,1, 'Jan/19', 3);
+insert into leave values(2,3, 'Jan/19', 4);
+insert into leave values(3,2, 'Jan/19', 5);
+insert into leave values(4,6, 'Jan/19', 3);
+insert into leave values(5,4, 'Jan/19', 1);
---
+++ README.md
@@ -0,0 +1,23 @@
+
+echo "# DBMS-Salary-Manager" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/archttt/DBMS-Salary-Manager.git
+git push -u origin main
+
+
+## DBMS Mini Project
+DBMS Lab, DTU(4th semester)
+
+Designed and developed a salary management system, implemented using PL/SQL and oracle database
+
+Report consists of-
+* SQL Queries
+* Functions
+* Triggers
+* Procedures
+* ER Diagram
+* Schema Diagram
+* Implementation
